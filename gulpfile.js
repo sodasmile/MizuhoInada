d6567cf5262cd9a3f8e4a9775184ba9e53d10692
Eivind Lysne <eivind.lysne@bouvet.no>
2015-11-06 18:24:55 +0100

    more buildscript

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var babelify = require('babelify');
 
 gulp.task('browserify', function () {
     return browserify('./src/app.js')
+    .transform(babelify)
     .bundle()
     //Pass desired output filename to vinyl-source-stream
     .pipe(source('bundle.js'))
