06e5ea0bb9a64d111674141f55ce35e782bcffd9
Anders Smestad <anders.smestad@bouvet.no>
2015-11-07 00:25:09 +0100

    2014 versjon viser hardkodet markør på oscarsborg

--- curl/postPost.sh
+++ curl/postPost.sh
@@ -3,4 +3,4 @@ curl -X POST --header "Content-Type: application/json" \
              --header "LagKode: nedover_lia_triller_en_traktor" \
              --header "DeltakerKode: 40041446" \
              "https://bbr2015.azurewebsites.net/api/GameService" \
-             -d "{'postKode': '12345', 'bruktVåpen': 'BOMBE'}"
+             -d "{'postKode': 'Post1', 'bruktVåpen': 'BOMBE'}"
