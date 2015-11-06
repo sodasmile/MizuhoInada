e703196cbe53c8d59929b9efa8584f55709e3c5d
Anders Smestad <anders.smestad@bouvet.no>
2015-11-06 19:24:41 +0100

    Legger til fler eksempler

--- curl/getGameState.sh
+++ curl/getGameState.sh
@@ -1,6 +1,6 @@
-curl -X POST --header "Content-Type: application/json" \
+curl -X GET --header "Content-Type: application/json" \
              --header "Accept: application/json" \
              --header "LagKode: nedover_lia_triller_en_traktor" \
              --header "DeltakerKode: 40041446" \
-             "https://bbr2015.azurewebsites.net/api/Meldinger" \
-             -d "{'tekst':'tjobing'}"
+             "https://bbr2015.azurewebsites.net/api/GameStateFeed" \
+             |  python -m json.tool
--- curl/getMeldinger.sh
+++ curl/getMeldinger.sh
@@ -2,4 +2,5 @@ curl -X GET --header "Content-Type: application/json" \
             --header "Accept: application/json" \
             --header "LagKode: nedover_lia_triller_en_traktor" \
             --header "DeltakerKode: 40041446" \
-            https://bbr2015.azurewebsites.net/api/Meldinger
+            https://bbr2015.azurewebsites.net/api/Meldinger \
+            | python -m json.tool
