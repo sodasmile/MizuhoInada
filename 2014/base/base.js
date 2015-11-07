abf918d58bb73dcaa782cb5e1b0b90e81b4d964c
Anders Smestad <anders.smestad@bouvet.no>
2015-11-07 02:39:18 +0100

    Meldingstream på kartsiden, pluss reformatering av index.html

--- 2014/base/base.js
+++ 2014/base/base.js
@@ -4,15 +4,17 @@ $(document).ready(boot);
 
 function boot() {
     var map = initializeMap();
-    setCurrentPosition(map);
+    //setCurrentPosition(map);
+    myLocation = new google.maps.LatLng("59.676229", "010.606291");
+    map.setCenter(myLocation);
 
-    rattata();
+    oppdaterSide();
 
-    setInterval(rattata, 6000);
+    setInterval(oppdaterSide, 6000);
 }
 
 
-function rattata() {
+function oppdaterSide() {
 
     for (i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
@@ -44,7 +46,7 @@ function rattata() {
 
 
             var dot;
-            switch (person.deltakerId){
+            switch (person.deltakerId) {
                 case "JAVA_2-1":
                     dot = "GREEN";
                     break;
@@ -63,6 +65,22 @@ function rattata() {
 
             }
             setMyPositionMarker(map, person.latitude, person.longitude, true, dot);
+        }
+    });
+
+    $.ajax({
+        url: "https://bbr2015.azurewebsites.net/api/Meldinger", headers: {
+            "Content-Type": "application/json", "Accept": "application/json", "LagKode": "nedover_lia_triller_en_traktor", "DeltakerKode": "40041446"
+        }
+
+    }).then(function (data) {
+        meldinger = data.meldinger;
+        melding = document.getElementById('messages')
+        melding.innerText = '';
+        for (i = 0; i < meldinger.length; i++) {
+            var meldinger2 = meldinger[i];
+            meldingsTekst = meldinger2.melding;
+            melding.innerText = melding.innerText + '\n' + meldingsTekst + " " + meldinger2["tidspunktUtc"];
         }
     });
 };
@@ -99,32 +117,29 @@ function setMyPositionMarker(map, latitude, longitude, bounce, dot) {
 
     switch (dot) {
         case "RED":
-            icon= 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+            icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
             break;
         case "GREEN":
-            icon= 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+            icon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
             break;
         case "BLUE":
-            icon= 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+            icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
             break;
         case "YELLOW":
-            icon= 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
+            icon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
             break;
         case "PINK":
         default:
-            icon= 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png';
+            icon = 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png';
             break;
     }
 
     myLocation = new google.maps.LatLng(latitude, longitude);
     var marker = new google.maps.Marker({
-        position: myLocation, animation: animation,
-        icon: icon,
-        tooltip: "Fiskedisk"
+        position: myLocation, animation: animation, icon: icon, tooltip: "Fiskedisk"
     });
 
     marker.setMap(map);
-    map.setCenter(myLocation);
 
     markers.push(marker);
 }
