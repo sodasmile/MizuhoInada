d12d97327f567363b91538e2a23f80b8215da4dc
Emilie Falkberger <emilie.falkberger@bouvet.no>
2015-11-07 01:24:21 +0100

    naa skjer det

--- 2014/base/base.js
+++ 2014/base/base.js
@@ -2,11 +2,8 @@ $(document).ready(function () {
     var map = initializeMap();
     setCurrentPosition(map);
     $.ajax({
-        url: "https://bbr2015.azurewebsites.net/api/GameStateFeed",
-        headers: { "Content-Type": "application/json",
-            "Accept": "application/json",
-            "LagKode": "nedover_lia_triller_en_traktor",
-            "DeltakerKode": "40041446"
+        url: "https://bbr2015.azurewebsites.net/api/GameStateFeed", headers: {
+            "Content-Type": "application/json", "Accept": "application/json", "LagKode": "nedover_lia_triller_en_traktor", "DeltakerKode": "40041446"
         }
 
     }).then(function (data) {
@@ -15,7 +12,21 @@ $(document).ready(function () {
             post = poster[i];
             var bounce = post.harRegistert;
             //alert("Post " + i + " " + bounce);
-            setMyPositionMarker(map, post.latitude, post.longitude, bounce);
+            setMyPositionMarker(map, post.latitude, post.longitude, bounce, "BLUE");
+        }
+    });
+
+    $.ajax({
+        url: "https://bbr2015.azurewebsites.net/api/PosisjonsService", headers: {
+            "Content-Type": "application/json", "Accept": "application/json", "LagKode": "nedover_lia_triller_en_traktor", "DeltakerKode": "40041446"
+        }
+
+    }).then(function (data) {
+        for (i = 0; i < data.length; i++) {
+            person = data[i];
+            //alert("Post " + i + " " + bounce);
+            setMyPositionMarker(map, person.latitude, person.longitude, true, "PINK");
+
         }
     });
 
@@ -25,8 +36,7 @@ $(document).ready(function () {
 function initializeMap() {
 
     var mapOptions = {
-        center: { lat: "59.676229", lng: "010.606291" },
-        zoom: 16
+        center: {lat: "59.676229", lng: "010.606291"}, zoom: 16
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     //setMyPositionMarker(map);
@@ -44,17 +54,34 @@ function setCurrentPosition(map) {
     }
 }
 
-function setMyPositionMarker(map, latitude, longitude, bounce) {
+function setMyPositionMarker(map, latitude, longitude, bounce, dot) {
+
     if (bounce) {
         animation = google.maps.Animation.DROP;
     } else {
         animation = google.maps.Animation.BOUNCE;
     }
+
+    switch (dot) {
+        case "RED":
+            icon= 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+            break;
+        case "GREEN":
+            icon= 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+            break;
+        case "BLUE":
+            icon= 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+            break;
+        case "PINK":
+        default:
+            icon= 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png';
+            break;
+    }
+
     myLocation = new google.maps.LatLng(latitude, longitude);
     var marker = new google.maps.Marker({
-        position: myLocation,
-        title: "Kor i svartre er eg?",
-        animation: animation
+        position: myLocation, animation: animation,
+        icon: icon
     });
 
 
